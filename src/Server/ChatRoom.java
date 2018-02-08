bc6999dde46993352193c6dd4295f5a0f57f5d9b
ageri <tonio_96@hotmail.Fr>
2018-02-08 19:10:05 +0100

    Correction delete - à tester

--- src/Server/ChatRoom.java
+++ src/Server/ChatRoom.java
@@ -1,5 +1,6 @@
 package Server;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ChatRoom {
@@ -45,18 +46,20 @@ public class ChatRoom {
 
                 //Problème d'accès concurrent
 
-                for (Map.Entry<Integer, User> entry : this.usersMap.usersMap.entrySet()) {
-                    User v = entry.getValue();
-                    v.addMessageToQueue(new Message(MessageType.ERROR, "Chatroom supprimée", user.getUserId()));
-                    v.changeRoom("default");
-                }
-
-//                this.usersMap.usersMap.forEach((Integer k, User v) -> {
-//                    v.changeRoom("default");
-//                });
-//                this.usersMap.usersMap.forEach((Integer k, User v) -> {
+//                for (Map.Entry<Integer, User> entry : this.usersMap.usersMap.entrySet()) {
+//                    User v = entry.getValue();
 //                    v.addMessageToQueue(new Message(MessageType.ERROR, "Chatroom supprimée", user.getUserId()));
+//                    v.changeRoom("default");
+//                }
+
+                HashMap<Integer, User> copy = new HashMap<Integer, User>(this.usersMap.usersMap);
+                copy.forEach((Integer k, User v) -> {
+//              this.usersMap.usersMap.forEach((Integer k, User v) -> {
+                    v.changeRoom("default");
 //                });
+//                this.usersMap.usersMap.forEach((Integer k, User v) -> {
+                    v.addMessageToQueue(new Message(MessageType.ERROR, "Chatroom supprimée", user.getUserId()));
+                });
                 return true;
             }
         }
